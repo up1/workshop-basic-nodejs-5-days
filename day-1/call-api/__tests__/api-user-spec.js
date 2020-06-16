9927b4355e4f48a9266cdd809f2df2126c3f25e6
up1 <somkiat.p@gmail.com>
2020-06-16 13:42:03 +0700

    Testing with nock

--- day-1/call-api/__tests__/api-user-spec.js
+++ day-1/call-api/__tests__/api-user-spec.js
@@ -7,19 +7,19 @@ test("Integration test", async () => {
 });
 
 test("Component test with Mock server", async () => {
-   nock("https://jsonplaceholder.cypress.io")
-    .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+  nock("https://jsonplaceholder.cypress.io")
+    .defaultReplyHeaders({ "access-control-allow-origin": "*" })
     .get("/users")
     .reply(200, [{}, {}, {}]);
-    const response = await getUsers();
-    expect(response.data.length).toEqual(3);
+  const response = await getUsers();
+  expect(response.data.length).toEqual(3);
 });
 
 test("Component test with 404", async () => {
   nock("https://jsonplaceholder.cypress.io")
-   .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-   .get("/users")
-   .reply(500);
-   const response = await getUsers();
-   expect(response.code).toEqual(404);
+    .defaultReplyHeaders({ "access-control-allow-origin": "*" })
+    .get("/users")
+    .reply(500);
+  const response = await getUsers();
+  expect(response.code).toEqual(404);
 });
